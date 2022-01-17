04ce1ef1f2916dae53ccd6dc73894d330e5367bc
noriakis <noriaki.sato53@gmail.com>
2022-01-17 19:22:56 +0900

    Update main.R

--- main.R
+++ main.R
@@ -1,13 +1,31 @@
+library(GeneSummary)
+library(tm)
+library(org.Hs.eg.db)
 
 ## List of entrez ID
-geneList <- c("1161")
-additionalRemve <- NA
+keggPathways <- org.Hs.egPATH2EG
+mappedKeys <- mappedkeys(keggPathways)
+keggList <- as.list(keggPathways[mappedKeys])
+geneList <- keggList$`05160`
+additionalRemove <- NA
 
 ## Using GeneSummary
 tb <- loadGeneSummary()
 
 ## Already performed rda with high frequency words
-load("allFreqGeneSummary.rda") 
+allDocsPre <- VCorpus(VectorSource(tb$Gene_summary))
+allDocsPre <- allDocsPre %>%
+    tm_map(FUN=content_transformer(tolower)) %>% 
+    tm_map(FUN=removeNumbers) %>%
+    tm_map(removeWords, stopwords::stopwords("english", "stopwords-iso")) %>%
+    tm_map(FUN=removePunctuation) %>%
+    tm_map(FUN=stripWhitespace)
+matAllPre <- as.matrix(TermDocumentMatrix(allDocsPre))
+matAllPreSorted <- sort(rowSums(matAllPre), decreasing=TRUE)
+allFreqGeneSummary <- data.frame(matAllPreSorted)
+allFreqGeneSummary$word <- rownames(allFreqGeneSummary)
+colnames(allFreqGeneSummary) <- c("freq","word")
+# load("allFreqGeneSummary.rda") 
 
 ## Filter high frequency words
 excludeFreq <- 5000
@@ -27,6 +45,9 @@ docs <- docs %>%
 if (prod(is.na(additionalRemove))!=1){
     docs <- docs %>% tm_map(removeWords, additionalRemove)
 }
+docs <- TermDocumentMatrix(docs)
+mat <- as.matrix(docs)
+matSorted <- sort(rowSums(mat), decreasing=TRUE)
     
 ## Perform the same filtering for whole data
 allDocs <- VCorpus(VectorSource(tb$Gene_summary))
@@ -43,12 +64,17 @@ if (prod(is.na(additionalRemove))!=1){
 matAll <- as.matrix(TermDocumentMatrix(allDocs))
 matAllSorted <- sort(rowSums(matAll), decreasing=TRUE)
         
-## ORA
-name <- ## INSERT VALUE ##
-query <- as.numeric(matSorted[name])
-noquery <- sum(matSorted) - query
-queryAll <- as.numeric(matAllSorted[name])
-allwords <- sum(matAllSorted) - queryAll
 
-## p-value
-sum(dhyper(query:sum(matSorted), queryAll, allwords, sum(matSorted)))
+## ORA
+returnP <- function(name){
+    query <- as.numeric(matSorted[name])
+    noquery <- sum(matSorted) - query
+    queryAll <- as.numeric(matAllSorted[name])
+    allwords <- sum(matAllSorted) - queryAll
+
+    ## p-value
+    return(sum(dhyper(query:sum(matSorted), queryAll, allwords, sum(matSorted))))
+}
+
+sigs <- names(matSorted)[p.adjust(sapply(names(matSorted), returnP), "bonferroni")<0.05]
+sigs
